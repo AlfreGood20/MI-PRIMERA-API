62afa1f44ae0b5b4081b102112c438c67ef5ce58
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-07-19 16:43:27 -0600

    Refactorizado

--- src/main/java/com/spring/api/controlador/LibroControlador.java
+++ src/main/java/com/spring/api/controlador/LibroControlador.java
@@ -2,8 +2,13 @@ package com.spring.api.controlador;
 
 import com.spring.api.modelo.Libro;
 import com.spring.api.servicio.LibroServicio;
+import jakarta.validation.Valid;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,19 +25,44 @@ public class LibroControlador {
     private LibroServicio servicio;
 
     @PostMapping("/guardar")
-    public Libro guardar(@RequestBody Libro libro) {
-        return servicio.guardarLibro(libro);
+    public ResponseEntity<?> guardar(@Valid @RequestBody Libro libro, BindingResult errores) {
+
+        if (errores.hasErrors()) {
+
+            //LO MISMO QUE EL DE AUTOR PERO USANDO STREAM
+            Map<String, String> mapeoErrores = errores.getFieldErrors()
+            .stream()
+            .collect(Collectors.toMap(
+                error -> error.getField(), error -> error.getDefaultMessage()));
+                    //CAMPO                             //MENSAJE DE ERROR
+
+                    //STATUS 400
+            return ResponseEntity.badRequest().body(mapeoErrores);
+        }
+
+        servicio.guardarLibro(libro);
+        return ResponseEntity.status(201).build(); //STATUS 201
     }
 
     @GetMapping("/lista")
-    public List<Libro> listaLibros() {
-        return servicio.listarLibros();
+    public ResponseEntity<?> listaAutores(){
+        List<Libro>lista=servicio.listarLibros();
+
+        if(lista.isEmpty()){
+            return ResponseEntity.ok().body("No hay libros registrado"); //STATUS 200
+        }
+        return ResponseEntity.ok().build(); //STATUS 200
     }
-    
+
     @DeleteMapping("/eliminar")
-    public String eliminar(@RequestParam long id){
-        Libro libro= servicio.obtenerPorId(id);
+    public ResponseEntity<?> eliminar(@RequestParam long id) {
+        Libro libro = servicio.obtenerPorId(id);
+
+        if (libro==null) {
+            return ResponseEntity.notFound().build(); //STATUS 404 NO ENCONTRADO ESE RECURSO
+        }
+
         servicio.eliminarLibro(id);
-        return "Se elimino: "+libro.getNombre()+" año: "+libro.getAño();
+        return ResponseEntity.noContent().build(); //STATUS 204 ELIMINADO CORRECTAMENTE
     }
 }
